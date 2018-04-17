a8656d6963206c2ba83b85477ef8cfd471160de9
UrbanRude <urban.rude@gmail.com>
2018-04-17 12:00:11 -0500

    Curso basico de typeScript

--- src/app/basic/car.js
+++ src/app/basic/car.js
@@ -1,23 +1,17 @@
 class Car {
-
-    private _engine: string;
-
-    constructor(engine: string){
+    constructor(engine) {
         this._engine = engine;
     }
-    // por omision es publico
-    starEngine(){
+    starEngine() {
         console.log(`Start engine ${this._engine}`);
         alert(`Start engine ${this._engine}`);
     }
-
-    stopEngine(){
+    stopEngine() {
         alert(`Stop engine ${this._engine}`);
     }
 }
-
-window.onload = function(){
+window.onload = function () {
     var car = new Car("Buggatti");
     car.starEngine();
     car.stopEngine();
-}
+};
